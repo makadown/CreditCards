70d42d9396b1eee9f5b54a646a13e1a88403e282
makadown <maseflores79@gmail.com>
2021-12-20 14:02:38 -0500

    My First Test!

---
+++ CreditCards.DotNet6.UITests/CreditCardWebAppShould.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CreditCards.DotNet6.UITests
+{
+    public class CreditCardWebAppShould
+    {
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationPage()
+        {
+            using IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://localhost:44108/");
+            Assert.True(1 == 1);
+        }
+    }
+}
---
+++ CreditCards.UITests/CreditCardWebAppShould.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using Xunit;
+
+namespace CreditCards.UITests
+{
+    public class CreditCardWebAppShould
+    {
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                Assert.True(1 == 1);
+            }            
+        }
+    }
+}
